4ef508088ea95df4cfe4d030ff200fcc774058d2
gjouet83 <jouetguillaume@gmail.com>
2022-04-16 22:02:59 +0200

    Initialisation du projet

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const https = require('https');
+const app = require('./app');
+const fs = require('fs');
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.herewelaugh.com/privkey.pem');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/www.herewelaugh.com/cert.pem');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+const server = https.createServer(credentials, app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
