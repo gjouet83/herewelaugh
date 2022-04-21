0f3cf44cb1becf955b06ca823b45096ec5d2f166
gjouet83 <jouetguillaume@gmail.com>
2022-04-21 08:46:12 +0200

    development of user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,58 @@
 const express = require('express');
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
-const validate = require('../middleware/validateinputs');
+const validateInputs = require('../middleware/validateinputs');
 const multer = require('../middleware/multer');
 
 const router = express.router();
+
+router.get('/get', auth, userCtrl.getUsers);
+router.get('/get/:id', auth, userCtrl.getUser);
+router.post(
+  '/signup',
+  validateInputs.username,
+  validateInputs.email,
+  validateInputs.password,
+  userCtrl.signup
+);
+router.post(
+  '/login',
+  validateInputs.email,
+  validateInputs.password,
+  userCtrl.login
+);
+router.put(
+  '/update/login/:id',
+  auth,
+  validateInputs.email,
+  userCtrl.updateLogin
+);
+router.put(
+  '/update/password/:id',
+  auth,
+  validateInputs.password,
+  userCtrl.updatePassword
+);
+router.put(
+  '/update/profile/username/:id',
+  auth,
+  validateInputs.username,
+  userCtrl.updateUserProfile
+);
+router.put(
+  '/update/profile/describ/:id',
+  auth,
+  validateInputs.username,
+  userCtrl.updateUserProfile
+);
+router.put(
+  '/update/profile/avatar/:id',
+  auth,
+  multer,
+  validateInputs.username,
+  userCtrl.updateUserProfile
+);
+
+router.delete('/delete/:id', auth, userCtrl.deleteUserAccount);
+
+module.exports = router;
