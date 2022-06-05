bc5ad1ba6486b3ac878b610e7c0ba51800547141
gjouet83 <jouetguillaume@gmail.com>
2022-06-05 22:23:05 +0200

    Configure multer

--- backend/middleware/multer-post.js
+++ backend/middleware/multer-post.js
@@ -12,6 +12,17 @@ const MIME_TYPES = {
   'image/webp': 'webp',
   'image/avif': 'avif',
   'image/vnd.microsoft.icon': 'ico',
+  'video/3gpp': '3gp',
+  'video/3gpp2': '3gpp2',
+  'video/x-msvideo': 'avi',
+  'video/mp4': 'mp4',
+  'video/mpeg': 'mpeg',
+  'video/ogg': 'ogv',
+  'video/mp2t': 'ts',
+  'video/webm': 'webm',
+  'video/x-flv': 'flv',
+  'video/quicktime': 'mov',
+  'video/x-ms-wmv': 'wmv',
 };
 
 const storage = multer.diskStorage({
@@ -24,7 +35,7 @@ const storage = multer.diskStorage({
     //on crée un nom de fichier constitué du userId, de la date et de l'extention
     callback(
       null,
-      'userId-' + req.body.userId + 'avatar' + Date.now() + '.' + extension
+      'userId-' + req.body.userId + 'posts' + Date.now() + '.' + extension
     );
   },
 });
@@ -34,7 +45,7 @@ const uploadMedia = multer({
   fileFilter: (req, file, callback) => {
     if (
       !file.originalname.match(
-        /\.(jpeg|jpg|tif|tiff|png|svg|gif|bmp|webp|avif|ico)$/
+        /\.(jpeg|jpg|tif|tiff|png|svg|gif|bmp|webp|avif|ico|3gp|3gp2|avi|mp4|mpeg|ogv|ts|webm|flv|mov|wmv)$/
       )
     ) {
       return callback(new Error("Le fichier n'est pas valable."));
