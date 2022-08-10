0b73f3ae349d8882411ac7aec07da66107174724
gjouet83 <jouetguillaume@gmail.com>
2022-08-10 22:51:43 +0200

    init frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch></Switch>
+    </Router>
   </React.StrictMode>
 );
 
