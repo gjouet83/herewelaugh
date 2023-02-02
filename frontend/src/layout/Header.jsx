7340e8d63acb0e8fb620e51efd3c669e7b2b59d4
gjouet83 <jouetguillaume@gmail.com>
2023-02-02 21:38:52 +0100

    start design header

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,5 +1,17 @@
+import logo from '../assets/Logonew_transparent.webp';
+
 const Header = () => {
-  return <header>test</header>;
+  return (
+    <header>
+      <img className="logo__img" src={logo} alt="emojoi qui pleur de rire" />
+      <div className="user">
+        <div className="user__welcom">
+          <span>Bienvenue</span>
+        </div>
+        <nav></nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
